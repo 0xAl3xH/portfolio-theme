29d32dc4029bdcf086d4d13f95a2d77a8447e346
Alex Huang <alexh95@mit.edu>
2017-02-20 23:22:47 +0800

    Implement mobile collapsible menu

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,10 +1,13 @@
+var show = false;
 //Wait until all assets are loaded (images, fonts, etc...)
 $(window).on('load', function () {
   setupFixedNav();
+  setupHeaderTabMobile();
   $(window).resize(function () {
     setupFixedNav();
+    setupHeaderTabMobile();
+    console.log("resieze");
   });
-
   //Setup complete, reveal the page to the user. 
   makeVisible();
 });
@@ -14,6 +17,34 @@ $(window).on('load', function () {
  **/
 function setupFixedNav() {
   $('.wrapper_alex').css('margin-top', $('.header_alex').outerHeight(true));
+}
+
+/**
+ * Account for the height of the nav bar for the mobile header tab container
+ **/
+function setupHeaderTabMobile() {
+  $('.header_tab_container_mobile_alex').css('top', $('.header_alex').outerHeight(false));
+  //Unbind otherwise multiple handlers might be attached upon resize
+  $('.header_tab_button_mobile_alex').unbind().on('click',toggleMenuMobile);
+}
+
+/**
+ * Function for toggling visibility of the mobile navigation menu
+ **/
+function toggleMenuMobile() {
+  var menu = '.header_tab_container_mobile_alex';
+  var animated = $(menu).is(':animated')  
+  console.log(animated);
+  if (!animated) {
+    show = !show;
+    if (show) {
+      $(menu).css('overflow','scroll');
+      $(menu).animate({height: $( window ).height() -$('.header_alex').outerHeight(false)});
+    } else {
+      $(menu).css('overflow','hide');
+      $(menu).animate({height: 0});
+    } 
+  }
 }
 
 /**
